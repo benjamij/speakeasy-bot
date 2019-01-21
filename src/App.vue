19d1fbd003e21f0ec6ea2a79ddaec68688855c16
Benjamin Jakobus <benjamin.jakobus@gmail.com>
2019-01-21 16:14:23 -0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <input
+            v-model="message"
+            type="text">
+        <h2 class="message">{{ message }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'SpeakEasy!',
+        };
+    },
+};
+</script>
+
+<style>
+    .message {
+        color: red;
+    }
+</style>
