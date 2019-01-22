70d84717310e62a53deedd47c927f75d34ced484
Benjamin Jakobus <benjamij@users.noreply.github.com>
2019-01-22 12:34:36 -0200

    Revert "Bot launcher icon component"

--- src/App.vue
+++ src/App.vue
@@ -4,32 +4,20 @@
             v-model="message"
             type="text">
         <h2 class="message">{{ message }}</h2>
-        <se-bot-launcher-button v-bind="{toggleChatbox}"></se-bot-launcher-button>
     </div>
 </template>
 
 <script>
-    import BotLauncherComponent from './components/BotLauncher.vue';
-
-    export default {
-        data() {
-            return {
-                message: 'SpeakEasy!',
-                chatBoxVisibility: false
-            };
-        },
-        methods: {
-            toggleChatbox: function (event) {
-                this.chatBoxVisibility = !this.chatBoxVisibility;
-            }
-        },
-        components: {
-            'se-bot-launcher-button': BotLauncherComponent,
-        }
-    };
+export default {
+    data() {
+        return {
+            message: 'SpeakEasy!',
+        };
+    },
+};
 </script>
 
-<style scoped>
+<style>
     .message {
         color: red;
     }
