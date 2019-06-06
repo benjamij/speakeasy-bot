06254b41f99bb94d0ca607d04984418ef6c397a1
Benjamin Jakobus <benjamin.jakobus@gmail.com>
2019-06-06 11:55:03 -0300

    Fix webpack build
    Allow switching between production and development modes.

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,38 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const webpack = require('webpack');
 
-require('dotenv').config({path: './.env.production.local'});
-console.log(process.env.API_URL);
-module.exports = {
-    entry: './src/main.js',
-    module: {
-        rules: [
-            { test: /\.js$/, use: 'babel-loader' },
-            { test: /\.vue$/, use: 'vue-loader' },
-            { test: /\.css$/, use: ['vue-style-loader', 'css-loader']},
-            { 
-                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-                use: ['file-loader'],
-            }
+
+
+module.exports = (env, options) => {
+    console.log('Running in ' + options.mode + ' mode.');
+    if (options.mode === 'production') {
+        require('dotenv').config({path: './.env.production.local'});
+    } else {
+        require('dotenv').config({path: './.env.development.local'});
+    }
+    return {
+        entry: './src/main.js',
+        module: {
+            rules: [
+                { test: /\.js$/, use: 'babel-loader' },
+                { test: /\.vue$/, use: 'vue-loader' },
+                { test: /\.css$/, use: ['vue-style-loader', 'css-loader']},
+                { 
+                    test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+                    use: ['file-loader'],
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+            }),
+            new webpack.DefinePlugin({
+                'process.env': {
+                    'API_URL': JSON.stringify(process.env.API_URL)
+                }
+            }),
+            new VueLoaderPlugin(),
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'API_URL': JSON.stringify(process.env.API_URL)
-            }
-        }),
-        new VueLoaderPlugin(),
-    ]
+    }
 };
